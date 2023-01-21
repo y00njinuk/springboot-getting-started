1573c7a4cf9451d76245f6da5bc98a3f287f5fd4
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-01-21 17:17:34 +0900

    init

---
+++ src/main/java/com/tutorial/api/SpringbootGettingStartedApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGettingStartedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGettingStartedApplication.class, args);
+    }
+
+}
