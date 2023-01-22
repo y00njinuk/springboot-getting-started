e655133a4fb3eaa9a3dc7635f8c67f4c6229a536
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-01-23 07:24:47 +0900

    feat: add POST method "/hello" & "/request"
    - POST 메서드의 기본적인 설계방식을 바탕으로 정의 및 구현
    - in Chapter05

---
+++ src/main/java/com/tutorial/api/controller/PostController.java
@@ -0,0 +1,23 @@
+package com.tutorial.api.controller;
+
+import com.tutorial.api.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/post-api")
+public class PostController {
+
+    // RequestMapping을 활용하여 GET 메서드에 대한 응답을 정의
+    @RequestMapping(value = "/hello", method= RequestMethod.POST)
+    public String postHello() {
+
+        return "Hello World";
+    }
+
+    // PostMapping을 활용하여 매개변수를 전달하는 POST 메서드에 대한 응답을 정의
+    @PostMapping(value="/request")
+    public String postMemberDto(@RequestBody MemberDto memberDto) {
+
+        return memberDto.toString();
+    }
+}
