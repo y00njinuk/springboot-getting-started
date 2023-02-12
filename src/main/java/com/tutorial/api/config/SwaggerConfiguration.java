2529846bc4dcc8cb3864c5cb8b0e25851a3a78fc
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-02-12 16:46:05 +0900

    feat: add Configuration for Swagger
    - Swagger의 기본적인 설정을 위한 Configuration 클래스 정의
    - in Chapter05

---
+++ src/main/java/com/tutorial/api/config/SwaggerConfiguration.java
@@ -0,0 +1,31 @@
+package com.tutorial.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.springboot.api")) // Swagger에서 스캔할 패키지의 범위
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot Open API Test with Swagger")
+                .description("설명 부분")
+                .version("1.0.0")
+                .build();
+    }
+}
