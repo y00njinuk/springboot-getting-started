d10c0c4610929f921aa58946fa32d9fccc4780c3
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-02-12 18:47:40 +0900

    feat: add logging in GET Method
    - GET Method 호출 시 각 메서드 별로 로그 기록
    - in Chapter05

--- src/main/java/com/tutorial/api/controller/GetController.java
+++ src/main/java/com/tutorial/api/controller/GetController.java
@@ -2,6 +2,8 @@ package com.tutorial.api.controller;
 
 import com.tutorial.api.dto.MemberDto;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,12 +12,13 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class GetController {
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
 
     // RequestMapping을 활용하여 GET 메서드에 대한 응답을 정의
     @RequestMapping(value="/hello", method= RequestMethod.GET)
     @ApiOperation(value="GET 메서드 예제 (1)", notes="기본적인 GET Method")
     public String getHello() {
-
+        LOGGER.info("getHello 메서드가 호출되었습니다.");
         return "Hello World";
     }
 
@@ -23,7 +26,8 @@ public class GetController {
     @GetMapping(value="/request/{variable}")
     @ApiOperation(value="GET 메서드 예제 (2)", notes="@PathVariable을 활용한 GET Method")
     public String getRequestParam(@PathVariable("variable") String variable){
-
+        LOGGER.info("getRequestParam 메서드가 호출되었습니다.");
+        LOGGER.info("@PathVariable를 통해 들어온 값 : {}", variable);
         return variable;
     }
 
@@ -32,7 +36,8 @@ public class GetController {
     @ApiOperation(value="GET 메서드 예제 (3)", notes="@RequestParam을 활용한 GET Method")
     public String getRequestParams(@RequestParam Map<String, String> params) {
         JSONObject jsonObject = new JSONObject(params);
-
+        LOGGER.info("getRequestParams 메서드가 호출되었습니다.");
+        LOGGER.info("@RequestParam를 통해 들어온 값 : {}", params);
         return jsonObject.toString();
     }
 
@@ -40,6 +45,8 @@ public class GetController {
     @GetMapping(value = "/request3")
     @ApiOperation(value="GET 메서드 예제 (4)", notes="DTO을 활용한 GET Method")
     public MemberDto getRequestParamsToDto(MemberDto memberDto) {
+        LOGGER.info("getRequestParamsToDto 메서드가 호출되었습니다.");
+        LOGGER.info("DTO 값 : {}", memberDto.toString());
         return memberDto;
     }
 }
