18a07e5e51604d57f7698ece3767382059b257b7
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-02-12 16:48:30 +0900

    feat: add annotation for Swagger api in GET Method
    - GET 메서드 내에서 Swagger 명세서 작성을 위한 어노테이션 추가
    - in Chapter05

--- src/main/java/com/tutorial/api/controller/GetController.java
+++ src/main/java/com/tutorial/api/controller/GetController.java
@@ -1,6 +1,7 @@
 package com.tutorial.api.controller;
 
 import com.tutorial.api.dto.MemberDto;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ public class GetController {
 
     // RequestMapping을 활용하여 GET 메서드에 대한 응답을 정의
     @RequestMapping(value="/hello", method= RequestMethod.GET)
+    @ApiOperation(value="GET 메서드 예제 (1)", notes="기본적인 GET Method")
     public String getHello() {
 
         return "Hello World";
@@ -19,6 +21,7 @@ public class GetController {
 
     // GetMapping을 활용하여 매개변수를 전달하는 GET 메서드에 대한 응답을 정의
     @GetMapping(value="/request/{variable}")
+    @ApiOperation(value="GET 메서드 예제 (2)", notes="@PathVariable을 활용한 GET Method")
     public String getRequestParam(@PathVariable("variable") String variable){
 
         return variable;
@@ -26,6 +29,7 @@ public class GetController {
 
     // GET 호출 시 request 파라미터(?key1=value1&key2=...)를 참고하여 Map 컬렉션에 관련 정보들을 저장
     @GetMapping(value = "/request2")
+    @ApiOperation(value="GET 메서드 예제 (3)", notes="@RequestParam을 활용한 GET Method")
     public String getRequestParams(@RequestParam Map<String, String> params) {
         JSONObject jsonObject = new JSONObject(params);
 
@@ -34,6 +38,7 @@ public class GetController {
 
     // GET 호출 시 request 파라미터(?name=value1&email=...)를 참고하여 DTO에 관련 정보들을 저장
     @GetMapping(value = "/request3")
+    @ApiOperation(value="GET 메서드 예제 (4)", notes="DTO을 활용한 GET Method")
     public MemberDto getRequestParamsToDto(MemberDto memberDto) {
         return memberDto;
     }
