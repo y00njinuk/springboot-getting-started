e299a73fd1ca0b9db1affee02289df994f2b2650
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-02-17 00:31:59 +0900

    feat: add unit test for ProductService - getProduceTest, saveProduceTest
    - MockBean을 활용하여 기존의 ProductService와 동일한 테스트 진행
    - in Chapter07

---
+++ src/test/java/com/springboot/tutorial/ProductServiceTest2.java
@@ -0,0 +1,79 @@
+package com.springboot.tutorial;
+
+import com.springboot.tutorial.dto.ProductDto;
+import com.springboot.tutorial.dto.ProductResponseDto;
+import com.springboot.tutorial.repository.ProductRepository;
+import com.springboot.tutorial.repository.entity.Product;
+import com.springboot.tutorial.service.ProductService;
+import com.springboot.tutorial.service.impl.ProductServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(SpringExtension.class)
+@Import({ProductServiceImpl.class})
+@SpringBootTest
+public class ProductServiceTest2 {
+
+    @MockBean
+    ProductRepository productRepository;
+
+    @Autowired
+    ProductService productService;
+
+    @Test
+    public void getProductTest() {
+        // given
+        Product givenProduct = new Product();
+        givenProduct.setNumber(123L);
+        givenProduct.setName("펜");
+        givenProduct.setPrice(1000);
+        givenProduct.setStock(1234);
+
+        Mockito.when(productRepository.findById(123L))
+                .thenReturn(Optional.of(givenProduct));
+
+        // when
+        ProductResponseDto productResponseDto = productService.getProduct(123L);
+
+        // then
+        Assertions.assertEquals(productResponseDto.getNumber(), givenProduct.getNumber());
+        Assertions.assertEquals(productResponseDto.getName(), givenProduct.getName());
+        Assertions.assertEquals(productResponseDto.getPrice(), givenProduct.getPrice());
+        Assertions.assertEquals(productResponseDto.getStock(), givenProduct.getStock());
+
+        verify(productRepository).findById(123L);
+    }
+
+    @Test
+    void saveProductTest() {
+        // given
+        Mockito.when(productRepository.save(any(Product.class)))
+                .then(returnsFirstArg());
+
+        // when
+        ProductResponseDto productResponseDto = productService.saveProduct(
+                new ProductDto("펜", 1000, 1234));
+
+        // then
+        Assertions.assertEquals(productResponseDto.getName(), "펜");
+        Assertions.assertEquals(productResponseDto.getPrice(), 1000);
+        Assertions.assertEquals(productResponseDto.getStock(), 1234);
+
+        verify(productRepository).save(any());
+    }
+
+}
