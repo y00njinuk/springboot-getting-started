464fa43f64ef7c7eb4e40148978a7ae2a5ab9de4
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-02-16 21:44:01 +0900

    refactor: add annotation to ProductDto
    - 테스트를 위한 어노테이션 추가
    - in Chapter07

--- src/main/java/com/springboot/tutorial/dto/ProductDto.java
+++ src/main/java/com/springboot/tutorial/dto/ProductDto.java
@@ -1,11 +1,13 @@
 package com.springboot.tutorial.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+@Builder
 @Getter
 @Setter
+@ToString
+@RequiredArgsConstructor
+@EqualsAndHashCode
 @AllArgsConstructor // 클래스의 모든 필드를 파라미터로 가지는 생성자 생성
 public class ProductDto {
     private String name;
