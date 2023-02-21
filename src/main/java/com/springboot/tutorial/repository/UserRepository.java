e9818fc101d4abe6a74d250b16f78e7fca6d7cbf
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-02-22 00:12:58 +0900

    feat: define interface UserRepository
    - 사용자 정보 레포지토리 추가
    - in Chapter12

---
+++ src/main/java/com/springboot/tutorial/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.tutorial.repository;
+
+import com.springboot.tutorial.repository.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User getByUid(String uid);
+}
