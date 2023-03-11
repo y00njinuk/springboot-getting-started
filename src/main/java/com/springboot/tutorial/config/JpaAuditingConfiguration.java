b455246fda68a640c0022b6d5aec71f71457da86
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-03-11 18:42:40 +0900

    feat: add configuration for JPA Auditing
    - JPA Auditing 기능을 적용하기 위한 configuration 클래스 정의
    - in Chapter08

---
+++ src/main/java/com/springboot/tutorial/config/JpaAuditingConfiguration.java
@@ -0,0 +1,10 @@
+package com.springboot.tutorial.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+
+}
