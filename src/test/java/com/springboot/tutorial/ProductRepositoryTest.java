0bbb1d246ce4a5c681ea8f775c1316165706b12d
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-03-11 16:31:01 +0900

    test: add unit test for ProductRepository using QueryDSL
    - QueryDSL 메서드 테스트 추가
    - in Chapter08

--- src/test/java/com/springboot/tutorial/ProductRepositoryTest.java
+++ src/test/java/com/springboot/tutorial/ProductRepositoryTest.java
@@ -1,8 +1,13 @@
 package com.springboot.tutorial;
 
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.springboot.tutorial.repository.ProductRepository;
 import com.springboot.tutorial.repository.entity.Product;
+import com.springboot.tutorial.repository.entity.QProduct;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +18,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -24,16 +31,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
              // @Transactional 어노테이션을 포함하고 있어서 테스트 코드 종료 시 데이터베이스가 롤백된다.
              // 기본값으로 임베디드 데이터베이스를 사용한다. 다른 데이터베이스는 별도의 설정을 거쳐서 사용 가능하다.
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // 해당 설정 적용 시 애플리케이션에서 실제로 사용하는 데이터베이스로 테스트로 변경 가능
+@Transactional
 public class ProductRepositoryTest {
+    @PersistenceContext
+    EntityManager entityManager;
+
     @Autowired
     private ProductRepository productRepository;
 
-    @Before
+    @BeforeEach
     void setUp() {
         List<Product> products = Arrays.asList(
                 new Product(1L, "펜", 1000, 100, LocalDateTime.now(), LocalDateTime.now()),
-                new Product(2L, "펜", 1000, 100, LocalDateTime.now(), LocalDateTime.now()),
-                new Product(3L, "펜", 1000, 100, LocalDateTime.now(), LocalDateTime.now())
+                new Product(2L, "펜", 5000, 300, LocalDateTime.now(), LocalDateTime.now()),
+                new Product(3L, "펜", 500, 50, LocalDateTime.now(), LocalDateTime.now())
 
         );
         productRepository.saveAll(products);
@@ -93,4 +104,76 @@ public class ProductRepositoryTest {
         Page<Product> productPage = productRepository.findByName("펜", PageRequest.of(0, 2));
         System.out.println(productPage.get().count()); // 2
     }
+
+    @Test
+    @Transactional
+    @DisplayName("QueryDSL-JPAQuery을 활용하여 쿼리를 생성하고 결과를 확인한다.")
+    void queryDslTest() {
+        JPAQuery<Product> query = new JPAQuery<>(entityManager);
+        QProduct qProduct = QProduct.product;
+
+        List<Product> productList = query
+                .from(qProduct)
+                .where(qProduct.name.eq("펜"))
+                .orderBy(qProduct.price.asc())
+                .fetch();
+
+        printProudctList(productList);
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("QueryDSL-JPAQueryFactory을 활용하여 쿼리를 생성하고 결과를 확인한다.")
+    void queryDslTest2() {
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(entityManager);
+        QProduct qProduct = QProduct.product;
+
+        List<Product> productList = jpaQueryFactory.selectFrom(qProduct)
+                .where(qProduct.name.eq("펜"))
+                .orderBy(qProduct.price.asc())
+                .fetch();
+
+        printProudctList(productList);
+
+        // 전체 칼럼이 아닌 일부 칼럼만을 조회하는 것도 가능하다.
+
+        List<String> productNameList = jpaQueryFactory
+                .select(qProduct.name)
+                .from(qProduct)
+                .where(qProduct.name.eq("펜"))
+                .orderBy(qProduct.price.asc())
+                .fetch();
+
+        for (String productName : productNameList) {
+            System.out.println("----------------------------");
+            System.out.println("Product Name : " + productName);
+            System.out.println("----------------------------");
+        }
+
+        List<Tuple> productTupleList = jpaQueryFactory
+                .select(qProduct.name, qProduct.price)
+                .from(qProduct)
+                .where(qProduct.name.eq("펜"))
+                .orderBy(qProduct.price.asc())
+                .fetch();
+
+        for (Tuple productTuple : productTupleList) {
+            System.out.println("----------------------------");
+            System.out.println("Product Name : " + productTuple.get(qProduct.name));
+            System.out.println("Product Price : " + productTuple.get(qProduct.price));
+            System.out.println("----------------------------");
+        }
+    }
+
+    private void printProudctList(List<Product> productList) {
+        for (Product product : productList) {
+            System.out.println("----------------------------");
+            System.out.println("Product Number : " + product.getNumber());
+            System.out.println("Product Name : " + product.getName());
+            System.out.println("Product Price : " + product.getPrice());
+            System.out.println("Product Stock : " + product.getStock());
+            System.out.println("----------------------------");
+        }
+    }
+
 }
