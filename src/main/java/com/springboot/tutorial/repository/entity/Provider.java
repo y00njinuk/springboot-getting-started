3c6b840df203f2b0d19fb8a8a65e17b26d062295
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-03-12 19:35:46 +0900

    feat: add new OrphanRemove option to Provider entity
    - 고아 객체 제거 옵션을 Provider 엔티티에 추가
    - in Chapter09

--- src/main/java/com/springboot/tutorial/repository/entity/Provider.java
+++ src/main/java/com/springboot/tutorial/repository/entity/Provider.java
@@ -22,6 +22,6 @@ public class Provider extends BaseEntity {
 
     // mappedBy로 설정된 필드는 실제 데이터베이스 컬럼에는 변경 및 적용되지 않는다.
     // 즉, 양방향 연관관계 일 때 RDBMS의 형식처럼 사용하기 위함이다.
-    @OneToMany(mappedBy = "provider", cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "provider", cascade = CascadeType.PERSIST, fetch = FetchType.EAGER, orphanRemoval = true)
     private List<Product> product = new ArrayList<>();
 }
