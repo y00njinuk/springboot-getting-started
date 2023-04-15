61d9dc309b1ab5bce01cf67307808ede37b96a2f
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-04-16 01:49:07 +0900

    feat: authenticate request using filter
    - OncePerRequestFilter를 상속받은 필터 클래스 정의 및 구현
    - JWT 토큰 값 추출 및 검증에 사용
    - 토큰이 유효하면 Authenticateion 객체를 생성하여 SecurityContextHolder에 추가
    - in Chapter12

---
+++ src/main/java/com/springboot/tutorial/config/security/JwtAuthenticationFilter.java
@@ -0,0 +1,43 @@
+package com.springboot.tutorial.config.security;
+
+import com.springboot.tutorial.component.JwtTokenProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 또는 GenericFilterBean을 상속받아서 구현하여도 된다.
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request);
+        LOGGER.info("[doFilterInternal] Request URL : {}", request.getRequestURI());
+        LOGGER.info("[doFilterInternal] JWT 토큰 값 추출 : {}", token);
+
+
+        LOGGER.info("[doFilterInternal] JWT 토큰 유효성 체크");
+        if(token != null && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
