5bf1a3214810f49538da6ecc5871debdf5cd5d54
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-04-16 01:56:54 +0900

    refactor: add Annotation and Logger for test in Swagger
    - Swagger UI에서 테스트 진행을 위한 어노테이션 추가
    - 비즈니스 로직에 대한 모니터링 목적의 로그 추가
    - in Chapter12

--- src/main/java/com/springboot/tutorial/controller/ProductController.java
+++ src/main/java/com/springboot/tutorial/controller/ProductController.java
@@ -1,48 +1,98 @@
 package com.springboot.tutorial.controller;
 
+
 import com.springboot.tutorial.dto.ChangeProductNameDto;
 import com.springboot.tutorial.dto.ProductDto;
 import com.springboot.tutorial.dto.ProductResponseDto;
 import com.springboot.tutorial.service.ProductService;
-import lombok.RequiredArgsConstructor;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/product")
-@RequiredArgsConstructor // final 필드를 파라미터로 가지는 생성자 생성
 public class ProductController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
     private final ProductService productService;
 
-    @GetMapping
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping()
     public ResponseEntity<ProductResponseDto> getProduct(Long number) {
+        long currentTime = System.currentTimeMillis();
+        LOGGER.info("[getProduct] request Data :: productId : {}", number);
         ProductResponseDto productResponseDto = productService.getProduct(number);
 
+        LOGGER.info(
+                "[getProduct] response Data :: productId : {}, productName : {}, productPrice : {}, productStock : {}",
+                productResponseDto.getNumber(), productResponseDto.getName(),
+                productResponseDto.getPrice(), productResponseDto.getStock());
+        LOGGER.info("[getProduct] Response Time : {}ms", System.currentTimeMillis() - currentTime);
         return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
     }
 
-    @PostMapping
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
+    })
+    @PostMapping()
     public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductDto productDto) {
+        long currentTime = System.currentTimeMillis();
         ProductResponseDto productResponseDto = productService.saveProduct(productDto);
 
+        LOGGER.info("[createProduct] Response Time : {}ms", System.currentTimeMillis() - currentTime);
         return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
     }
 
-    @PutMapping
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
+    })
+    @PutMapping()
     public ResponseEntity<ProductResponseDto> changeProductName(
             @RequestBody ChangeProductNameDto changeProductNameDto) throws Exception {
+        long currentTime = System.currentTimeMillis();
+        LOGGER.info("[changeProductName] request Data :: productNumber : {}, productName : {}",
+                changeProductNameDto.getNumber(), changeProductNameDto.getName());
+
         ProductResponseDto productResponseDto = productService.changeProductName(
                 changeProductNameDto.getNumber(),
                 changeProductNameDto.getName());
 
+        LOGGER.info(
+                "[changeProductName] response Data :: productNumber : {}, productName : {}, productPrice : {}, productStock : {}",
+                productResponseDto.getNumber(), productResponseDto.getName(),
+                productResponseDto.getPrice(), productResponseDto.getStock());
+        LOGGER.info("[changeProductName] response Time : {}ms",
+                System.currentTimeMillis() - currentTime);
         return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
     }
 
-    @DeleteMapping
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
+    })
+    @DeleteMapping()
     public ResponseEntity<String> deleteProduct(Long number) throws Exception {
+        long currentTime = System.currentTimeMillis();
+        LOGGER.info("[deleteProduct] request Data :: productNumber : {}", number);
+
         productService.deleteProduct(number);
 
-        return ResponseEntity.status(HttpStatus.OK).body("정상적으로 제거되었습니다,");
+        LOGGER.info("[deleteProduct] response Time : {}ms",
+                System.currentTimeMillis() - currentTime);
+        return ResponseEntity.status(HttpStatus.OK).body("정상적으로 삭제되었습니다.");
     }
 }
